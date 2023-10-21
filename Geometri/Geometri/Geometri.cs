b7d73e0aeff24fd892c4d1839bd19a4467797896
Parmis Morshedi <parmis76.morshedi@gmail.com>
2023-10-22 00:08:34 +0200

    Add project files.

---
+++ Geometri/Geometri/Geometri.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    public class Geometri
+    {
+        //polymorfism
+        public virtual double Area()
+        {
+            return 0.0;
+        }
+
+    }
+    public class Rectangle : Geometri
+    {
+        public int Length {  get; set; }  
+        public int Width { get; set; }
+
+        public Rectangle()     
+        
+        {
+            Length=7;
+            Width = 8;
+        
+        }
+       
+        public override double Area()
+        {
+            return Length* Width;
+        }
+
+
+    }
+    public class Square : Geometri 
+    {
+        public int Side { get; set; }
+
+        public Square() 
+        {
+            Side = 4;
+        
+        }
+
+
+        public override double Area()
+        {
+            return Side* Side;
+        }
+    }
+    public class Circle : Geometri
+    {
+        public int Radius { get; set; }
+        public Circle() 
+        {
+            Radius = 3;
+        }
+    
+    public override double Area()
+        {
+            return Math.PI* Radius* Radius;
+        }
+    }
+    public class Parallellogram : Geometri 
+    {
+        public int BaseLength { get; set; }
+        public int Height { get; set; }
+
+        public Parallellogram() 
+        {
+            BaseLength = 9;
+            Height = 2;
+        
+        }
+        public override double Area()
+        {
+            return BaseLength* Height;
+        }
+    }
+    public class Ellipse : Geometri 
+    {
+        public int MajorAxis { get; set; }
+        public int MinorAxis { get; set; }
+
+        public Ellipse() 
+        {
+            MajorAxis = 3;
+            MinorAxis = 6;
+        
+        }
+        public override double Area()
+        {
+            return Math.PI* MajorAxis* MajorAxis;
+        }
+    }
+
+}
---
+++ Geometri/Geometri/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Square square = new Square();
+           Console.WriteLine($"{square.Area()}: is the area of the square. ");
+
+            Circle circle = new Circle();
+            Console.WriteLine($"{circle.Area()}: is the area of the circle");
+
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine($"{rectangle.Area()}: is the area of the rectangle");
+
+            Parallellogram parallellogram = new Parallellogram();
+            Console.WriteLine($"{parallellogram.Area()}: is the area of the parallellogram. ");
+
+            Ellipse ellipse = new Ellipse();
+            Console.WriteLine($"{ellipse.Area()}: is the area of the ellipse. ");
+
+
+
+
+        }
+    }
+}
